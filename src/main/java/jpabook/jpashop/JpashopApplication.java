ced8ed27fca97c1f24df5884b7ee707b3cdb0cde
Torres-09 <dla612@naver.com>
2022-07-02 04:36:45 +0900

    Lombok 플러그인

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+// 		Lombok 플러그인 사용시 @getter @setter 사용으로 get, set 메소드 자동 생성
+//		Hello hello = new Hello();
+//		hello.setData("hello");
+//		String data = hello.getData();
+//		System.out.println("data = " + data);
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
