7c352a68e71abe5aec1e8dccd7808296f22fa7d4
Torres-09 <dla612@naver.com>
2022-07-29 18:58:38 +0900

    주문 기능 테스트

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -58,31 +58,33 @@ public class OrderServiceTest {
     public void 상품주문_재고수량초과() throws Exception{
         //given
         Member member = createMember();
-        Item book = createBook("시골 JPA", 10000, 10);
+        Item item = createBook("시골 JPA", 10000, 10);
+
+        int orderCount =11;
 
         //when
+        orderService.order(member.getId(), item.getId(), orderCount);
 
         //then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
     }
 
     @Test
     public void 주문취소() throws Exception{
         //given
-
-        //when
-
-        //then
-    }
-
-    @Test
-    public void 상품주문_재고수량초과() throws Exception{
-        //given
         Member member = createMember();
-        Item book = createBook("시골 JPA", 10000, 10);
+        Item item = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
 
         //when
+        orderService.cancelOrder(orderId);
 
         //then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals("주문 취소 시 상태는 cancel이다.", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야한다.", 10 ,item.getStockQuantity());
     }
 
     private Item createBook(String name, int price, int stockQuantity) {
